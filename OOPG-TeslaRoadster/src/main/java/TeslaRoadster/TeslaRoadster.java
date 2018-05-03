ff124b1aadf85f384d5a588b3c418752d4c83c51
janwil <janwil@LAPTOP_JANWIL>
2018-05-03 18:26:10 +0200

    Initial commit

---
+++ OOPG-TeslaRoadster/src/main/java/TeslaRoadster/TeslaRoadster.java
@@ -0,0 +1,29 @@
+package TeslaRoadster;
+
+import nl.han.ica.OOPDProcessingEngineHAN.Engine.GameEngine;
+
+@SuppressWarnings("serial")
+public class TeslaRoadster extends GameEngine {
+
+	public TeslaRoadster() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setupGame() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
